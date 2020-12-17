ab0479d55859d0dd52b6864a25f3dcded2f957a5
QQ <71001778+Quuuod@users.noreply.github.com>
2020-12-17 16:19:33 +0300

    валидация

--- Proect.js
+++ Proect.js
@@ -1,6 +1,14 @@
 let btn = document.querySelector('button');
 let textarea = document.querySelector('textarea');
-btn.onclick = function() {
-    const JSONtoObj = JSON.parse(textarea.value);
-    console.log(JSONtoObj);
+btn.onclick = function () {
+    try {
+        if (textarea.value !== "") {
+            const JSONtoObj = JSON.parse(textarea.value);
+            console.log(JSONtoObj);
+        } else alert("Вы не добавили JSON файл!");
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            alert("Неправильный формат")
+        }
+    }
 };
