41d250e4a86022eed44795a30d1ca8fa12cf5413
QQ <71001778+Quuuod@users.noreply.github.com>
2020-12-20 17:03:58 +0300

    table

--- Proect.js
+++ Proect.js
@@ -5,29 +5,31 @@ btn.onclick = function () {
         if (textarea.value !== "") {
             const myjson = JSON.parse(textarea.value);
             let table = document.querySelector('table');
-            for (let elem in myjson) {
-                let tr = document.createElement('tr');
-      
-                let td1 = document.createElement('td');
-                td1.innerText =  elem;
-                tr.appendChild(td1);
+            for (i in myjson.CyberPunk) {
                 
+
+                let tr = document.createElement('tr');
+
+                let td1 = document.createElement('td');
+                td1.innerText = myjson.CyberPunk[i].name;
+                tr.appendChild(td1);
                 let td2 = document.createElement('td');
                 td2.innerText =  elem;
+                td2.innerText = myjson.CyberPunk[i].material;
                 tr.appendChild(td2);
-                
+
                 let td3 = document.createElement('td');
-                td3.innerText = elem.use;
+                td3.innerText = myjson.CyberPunk[i].use;
                 tr.appendChild(td3);
 
                 let td4 = document.createElement('td');
-                td4.innerText = elem.lifeTime;
+                td4.innerText = myjson.CyberPunk[i].lifeTime;
                 tr.appendChild(td4);
 
                 let td5 = document.createElement('td');
-                td5.innerText = elem.price;
+                td5.innerText = myjson.CyberPunk[i].price;
                 tr.appendChild(td5);
-                
+
                 table.appendChild(tr);
             }
         } else alert("Вы не добавили JSON файл!");
