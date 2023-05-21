aa556e7097e5465a272a13b6ccae38d4ad439812
minorio <mbajduk@mail.ru>
2023-05-21 21:42:59 +0300

    fixed bugs

--- Proect.js
+++ Proect.js
@@ -1,64 +1,66 @@
-let btn = document.querySelector("button");
-let textarea = document.querySelector("textarea");
-let centerBox = document.querySelector(".center-box");
-let table = document.querySelector("table");
+let btn = document.querySelector('button');
+let textarea = document.querySelector('textarea');
+let centerBox = document.querySelector('.center-box');
+let table = document.querySelector('table');
 let myjson = {};
-const modal = document.querySelector(".modal-box");
-const closeButton = document.querySelector(".close-button");
-const modalTable = document.querySelector(".modal-content table");
+const modal = document.querySelector('.modal-box');
+const closeButton = document.querySelector('.close-button');
+const modalTable = document.querySelector('.modal-content table');
 
 window.onload = function () {
-  table.onclick = function (event) {
-    if (event.target.className === "view") {
-      modalTable.innerHTML = "";
+	table.onclick = function (event) {
+		if (event.target.className === 'view') {
+			modalTable.innerHTML = '';
 
-      let trHead = document.createElement("tr");
-      let trBody = document.createElement("tr");
+			let trHead = document.createElement('tr');
+			let trBody = document.createElement('tr');
 
-      const targetId = event.target.id;
-      const modalData = myjson.CyberPunk.find(
-        (item) => item.attachments.id == targetId
-      ).attachments.data;
-      const modalkeys = Object.keys(modalData);
-      const newModalTh = getModalTableRow(modalkeys, trHead);
-      const newModalTr = getModalTableRow(modalData, trBody);
+			const targetId = event.target.id;
+			const modalData = myjson.CyberPunk.find(
+				(item) => item.attachments.id == targetId
+			).attachments.data;
+			const modalkeys = Object.keys(modalData);
+			const newModalTh = getModalTableRow(modalkeys, trHead);
+			const newModalTr = getModalTableRow(modalData, trBody);
 
-      modalTable.appendChild(newModalTh);
-      modalTable.appendChild(newModalTr);
+			modalTable.appendChild(newModalTh);
+			modalTable.appendChild(newModalTr);
 
-      modal.classList.add("modal-visible");
-    }
-  };
+			modal.classList.add('modal-visible');
+		}
+	};
 
-  modal.onclick = function () {
-    modal.classList.remove("modal-visible");
-  };
+	modal.onclick = function () {
+		modal.classList.remove('modal-visible');
+	};
 
-  closeButton.onclick = function () {
-    modal.classList.remove("modal-visible");
-  };
+	closeButton.onclick = function () {
+		modal.classList.remove('modal-visible');
+	};
 
-  btn.onclick = function () {
-    myjson = JSON.parse(textarea.value);
+	btn.onclick = function () {
+		centerBox.classList.add('hide-center-box');
 
-    centerBox.classList.add("hide-center-box");
+		if (textarea.value !== '') {
+			myjson = JSON.parse(textarea.value);
+			const headTr = document.createElement('tr');
+			console.log(myjson);
+			const nameColumns = Object.keys(myjson);
+			const tableHeadColumn = getTableColumnName(nameColumns, headTr);
 
-    if (textarea.value !== "") {
-      const headTr = document.createElement("tr");
-      const nameColumns = Object.keys(myjson.CyberPunk[0]);
-      const tableHeadColumn = getTableColumnName(nameColumns, headTr);
+			table.appendChild(tableHeadColumn);
 
-      table.appendChild(tableHeadColumn);
+			const arrayName = Object.keys(myjson)[0];
 
-      for (i = 0; i < myjson.CyberPunk.length; i++) {
-        let tr = document.createElement("tr");
-        const newTr = getTableRows(myjson.CyberPunk[i], tr);
+			for (i = 0; i < myjson[arrayName].length; i++) {
+				let tr = document.createElement('tr');
+				const newTr = getTableRows(myjson[arrayName][i], tr);
 
-        table.appendChild(newTr);
-      }
-    } else {
-      alert("Вы не добавили JSON файл!");
-      centerBox.classList.remove("hide-center-box");
-    }
-  };
+				table.appendChild(newTr);
+			}
+		} else {
+			alert('Вы не ввели JSON-код!');
+			centerBox.classList.remove('hide-center-box');
+		}
+	};
 };
--- getTableParts.js
+++ getTableParts.js
@@ -1,50 +1,49 @@
 function getModalTableRow(Data, trow) {
-  return Object.keys(Data).reduce((acc, item) => {
-    let modaltd = document.createElement("td");
+	return Object.keys(Data).reduce((acc, item) => {
+		let modaltd = document.createElement('td');
 
-    modaltd.className = "modaltd";
-    modaltd.innerText = Data[item];
+		modaltd.className = 'modaltd';
+		modaltd.innerText = Data[item];
 
-    acc.appendChild(modaltd);
+		acc.appendChild(modaltd);
 
-    return acc;
-  }, trow);
+		return acc;
+	}, trow);
 }
 
 function getTableRows(data, row) {
-  console.log(data, row);
-  return Object.keys(data).reduce((acc, item) => {
-    let td = document.createElement("td");
+	return Object.keys(data).reduce((acc, item) => {
+		let td = document.createElement('td');
 
-    if (typeof data[item].data !== "object") {
-      td.className = "col";
-      td.innerText = data[item];
-    } else {
-      td.id = "btnview";
-      const buttonView = document.createElement("button");
-      buttonView.innerText = "view";
-      buttonView.id = data[item].id;
+		if (typeof data[item].data !== 'object') {
+			td.className = 'col';
+			td.innerText = data[item];
+		} else {
+			td.id = 'btnview';
+			const buttonView = document.createElement('button');
+			buttonView.innerText = 'view';
+			buttonView.id = data[item].id;
 
-      buttonView.className = "view";
+			buttonView.className = 'view';
 
-      td.appendChild(buttonView);
-    }
+			td.appendChild(buttonView);
+		}
 
-    acc.appendChild(td);
+		acc.appendChild(td);
 
-    return acc;
-  }, row);
+		return acc;
+	}, row);
 }
 
 function getTableColumnName(data, row) {
-  console.log(data, row);
-  return data.reduce((acc, item) => {
-    let th = document.createElement("th");
-    th.className = "thname";
-    th.innerText = item;
+	console.log(data, row);
+	return data.reduce((acc, item) => {
+		let th = document.createElement('th');
+		th.className = 'thname';
+		th.innerText = item;
 
-    acc.appendChild(th);
+		acc.appendChild(th);
 
-    return acc;
-  }, row);
+		return acc;
+	}, row);
 }
